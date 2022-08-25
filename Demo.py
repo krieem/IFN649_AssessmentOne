22a2625017394e1f6fb44a7c2cd3fe3e92fe9b7f
Abdulkarim Alzahrani <abdulkarim@alzahrani.me>
2022-08-25 14:56:24 +1000

    Clean up

--- Demo.py
+++ Demo.py
@@ -1,29 +1,18 @@
-from multiprocessing.connection import Client
-from posixpath import split
-from signal import signal, SIGTERM, SIGHUP, pause
 from time import sleep
-import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import serial
-import paho.mqtt.client as mqtt
 import os
 
 hostname="3.11.79.48"
 port="1883"
 mqttUser="ifn649"
 mqttPass="ifn649"
-com0 = "98:D3:C1:FD:BD:C3"
 com1 = "98:D3:B1:FD:C0:5B"
 com2 = "98:D3:91:FD:D9:E5"
 
 def init():
-    blue0 = os.path.exists("/dev/rfcomm0")
     blue1 = os.path.exists("/dev/rfcomm1")
     blue2 = os.path.exists("/dev/rfcomm2")
-    if blue0 == False:
-        os.system("sudo rfcomm bind rfcomm0 {}", com0)
-        print("Teensy 1 Initialized")
-        print()
     if blue1 == False:
         os.system("sudo rfcomm bind rfcomm1 {}", com1)
         print("Teensy 2 Initialized")
@@ -51,7 +40,7 @@ def collectData():
                     data.append(strserial)
                     break
                 else:
-                    print("Watting for connection")
+                    print("Witting for connection")
                     print("Retring in 1 second")
                     print()
                     sleep(1)
@@ -69,33 +58,33 @@ def collectData():
 def  demo():
     for i in range(5):
         print()
-        print(f"This demo will be reprated {i + 1} time out of 5")
+        print(f"This demo will be repeated {i + 1} time out of 5")
         sensors = collectData()
 
         print( "Temp : " + sensors[0])
         print( "Soil : " + sensors[1])
 
         DHT = sensors[0]
-        humi = ("Humidity " + DHT[9:16])
-        temp = ("Temperature " + DHT[30:36]+ "C")
-        heat = ("Heat " + DHT[59:65]+ "C")
+        humi = DHT[9:16]
+        temp = (DHT[30:36]+ "C")
+        heat = (DHT[59:65]+ "C")
         soil = sensors[1]
 
         mqtt_auth = { 'username': mqttUser, 'password': mqttPass } 
-        publish.single("ifn649", humi , hostname = hostname, auth = mqtt_auth)
-        publish.single("ifn649", temp , hostname = hostname, auth = mqtt_auth)
-        publish.single("ifn649", heat , hostname = hostname, auth = mqtt_auth)
-        publish.single("ifn649", soil , hostname = hostname, auth = mqtt_auth)
+        publish.single("Humidity", humi , hostname = hostname, auth = mqtt_auth)
+        publish.single("Temperature", temp , hostname = hostname, auth = mqtt_auth)
+        publish.single("Heat", heat , hostname = hostname, auth = mqtt_auth)
+        publish.single("Soil", soil , hostname = hostname, auth = mqtt_auth)
 
-        i+=1
+        #i+=1
 
 
 while True:
     print()
-    print("Connection initializing")
-    print("Full Demo for assessment One")
-    print("Initializing connction")
+    print("Connections initializing")
     init()
+    print("Full Demo for assessment One")
+    print("Initializing connctions completed")
     print()
     print("1) Start the Demo")
     print("2) Exit")
--- aws.py
+++ aws.py
@@ -3,54 +3,59 @@ from time import sleep
 import serial
 import paho.mqtt.client as mqtt
 import os
+import threading
 
 hostname="3.11.79.48"
 port=1883
 mqttUser="ifn649"
 mqttPass="ifn649"
+topics = ["Humidity", "Temperature", "Soil", "Heat" ]
+actor = serial.Serial("/dev/rfcomm0", 9600)
+
 def init():
     blue0 = os.path.exists("/dev/rfcomm0")
     if blue0 == False:
         os.system("sudo rfcomm bind rfcomm0 {}", com0)
         print("Teensy 1 Initialized")
         print()
+    actor
 
 def on_connect(client, userdata, flags, rc): # func for making connection print("Connected to MQTT")
     print("Connected to MQTT")
     print("Connection returned result: " + str(rc) )
-    client.subscribe("ifn649")
+    print()
+    for topic in topics:
+        client.subscribe(topic)
+        print("subscribed to : " + topic)
+    print()
+    
 
 def on_message(client, userdata, msg):
-    strmsg = msg.payload
-    strmsg.decode('utf-8').strip('b\'\'')
-    print(strmsg)
-    #print(msg.topic+" "+str(msg.payload))
+    print("Receving Data From AWS")
+    led('1')
+    print()
+    print(msg.topic+" "+str(msg.payload))
+    led('0')
 
 def led(soil):
-    actor = serial.Serial("/dev/rfcomm0", 9600)
-    sleep(2)
-    while True:
-        if actor.in_waiting == 0:
-            if (soil == '0'):
-                actor.write("0".encode())
-                print("LED is Green")
-                break
-            elif (soil != '0'):
-                actor.write("1".encode())
-                print("LED is Orange")
-                break
-        else:
-            print("Connection Error")
-            print("Retring in 1 second")
-            print()
-            sleep(1)
+    try:
+        if (soil == '1'):
+            actor.write("0".encode())
+            sleep(2)
+        elif (soil == '0'):
+            actor.write("1".encode())
+    except:
+        print("Connection Error")
+        print("Retring in 1 second")
+        print()
+        
+init()
 
 client = mqtt.Client() 
-client.on_connect = on_connect 
+client.on_connect = on_connect
+
 client.on_message = on_message
 client.username_pw_set(mqttUser, mqttPass)
 client.connect(hostname , port, 60)
 client.loop_forever()
-sleep(1)
 
-#led(sensors[1])
